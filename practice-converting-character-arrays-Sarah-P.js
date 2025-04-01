26a7b3caa5c71d6285a7b5997815ae02f11a0b3c
Sarah <skat1403@gmail.com>
2025-03-31 22:52:57 -0500

    finished ToDos

--- practice-converting-character-arrays-Sarah-P.js
+++ practice-converting-character-arrays-Sarah-P.js
@@ -22,3 +22,25 @@ Task 2: Write your own reverse messages
 strings and arrays dynamically so that you have a reverse output.
 3. Then log the messages.
 */
+
+let message1 = "yako eb ll'uoy dna ,gniog peeK !sgnittes fo yteirav a ni slliks gnidoc esu osla nac uoY !ti teg ll'uoy ,tsisrep uoy fi tub ,tsrif ta drah mees yam gnidoC"
+let message2 = "!ecitcarp htiw retteb teg ll‘uoy ,emit ekaT .tsrif ta drah leef lliw gnitirw edoc tuB"
+let message3 = "!elpoep rehto morf tnereffid on era uoy ,elbuort evah uoy fI .lanoisseforp a ekil leef ot evah t'nod uoY"
+let message4 = ".rettam llits yeht ,smargorp llams etirw ylno nac uoy fI .tnemom tcefrep eht rof tiaw t'noD .yadot trats tsuJ"
+
+let reversed1 = message1.split('').reverse().join('');
+console.log(reversed1);
+
+let reversed2 = message2.split('').reverse().join('');
+console.log(reversed2);
+
+let reversed3 = message3.split('').reverse().join('');
+console.log(reversed3);
+
+let reversed4 = message4.split('').reverse().join('');
+console.log(reversed4);
+
+let myMessage = "I will finish class successfully!";
+let myReverse = myMessage.split('').reverse().join('');
+console.log(myMessage);
+console.log(myReverse);
