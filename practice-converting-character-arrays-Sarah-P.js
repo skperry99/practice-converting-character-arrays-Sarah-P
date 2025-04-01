a6f4a3a627b0e18c8d9a8b49d9cf085798d912cb
Sarah <skperry99@users.noreply.github.com>
2025-03-31 20:23:18 -0500

    initial commit

---
+++ practice-converting-character-arrays-Sarah-P.js
@@ -0,0 +1,24 @@
+/*
+Instructions
+You are working as a software engineer for a secret agency that needs a way to
+encrypt and decrypt messages. The agency communicates using character
+arrays instead of plain text to ensure the messages are secure.
+Tasks
+Task 1: Decode the Following Reversed Messages
+Use .split(''), reverse.(‘’) and .join('') to convert messages between strings and
+arrays dynamically. Then log the messages.
+1. Message 1: " !yako eb ll'uoy dna ,gniog peeK !sgnittes fo yteirav a ni slliks
+gnidoc esu osla nac uoY !ti teg ll'uoy ,tsisrep uoy fi tub ,tsrif ta drah mees yam
+gnidoC"
+2. Message 2: "!ecitcarp htiw retteb teg ll‘uoy ,emit ekaT .tsrif ta drah leef lliw
+gnitirw edoc tuB"
+3. Message 3: "!elpoep rehto morf tnereffid on era uoy ,elbuort evah uoy fI
+.lanoisseforp a ekil leef ot evah t'nod uoY"
+4. Message 4: ".rettam llits yeht ,smargorp llams etirw ylno nac uoy fI .tnemom
+tcefrep eht rof tiaw t'noD .yadot trats tsuJ"
+Task 2: Write your own reverse messages
+1. First, write your own short messages of inspiration (without reversing them).
+2. Then, use .split(''), reverse.(‘’) and .join('') to convert messages between
+strings and arrays dynamically so that you have a reverse output.
+3. Then log the messages.
+*/
